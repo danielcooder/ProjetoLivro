e0e458d09af84354a826c698cfc91fb2eca96109
daniel cooder <danielcooder@gmail.com>
2024-08-07 16:55:04 -0300

    feat: projeto livro - conexoes entre classes

---
+++ src/main/java/org/example/Publicacao.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public interface Publicacao {
+
+    public void abrir();
+    public void fechar();
+    public void folhear(int p);
+
+    public void avancarPag();
+    public void voltarPag();
+
+
+}
